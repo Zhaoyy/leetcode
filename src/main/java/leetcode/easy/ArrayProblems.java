4462340760c10d6c0c8c0ed1215ee4aa9288bb0e
mislead <867986737@qq.com>
2018-01-17 10:40:50 +0800

    easy

--- src/main/java/leetcode/easy/ArrayProblems.java
+++ src/main/java/leetcode/easy/ArrayProblems.java
@@ -12,7 +12,27 @@ public class ArrayProblems {
 
   public static void main(String[] args) {
     ArrayProblems problems = new ArrayProblems();
-    System.out.println(problems.findDisappearedNumbersBetter(new int[] {4, 3, 2, 7, 8, 2, 3, 1}));
+    System.out.println(problems.findRadius(new int[] {1, 2, 3, 5}, new int[] {1, 5}));
+  }
+
+  /**
+   * https://leetcode.com/problems/heaters/description/
+   */
+  public int findRadius(int[] houses, int[] heaters) {
+    Arrays.sort(heaters);
+    int result = 0;
+
+    for (int house : houses) {
+      int index = Arrays.binarySearch(heaters, house);
+      if (index < 0) index = ~index;
+
+      int dist1 = index - 1 >= 0 ? house - heaters[index - 1] : Integer.MAX_VALUE;
+      int dist2 = index < heaters.length ? heaters[index] - house : Integer.MAX_VALUE;
+
+      result = Math.max(result, Math.min(dist1, dist2));
+    }
+
+    return result;
   }
 
   /**
