cf5ec45eeb6800e56c5f64b1f4ae15b634774f88
mislead <867986737@qq.com>
2018-05-10 16:20:28 +0800

    medium

--- src/main/java/leetcode/easy/ArrayProblems.java
+++ src/main/java/leetcode/easy/ArrayProblems.java
@@ -47,6 +47,29 @@ public class ArrayProblems {
   }
 
   /**
+   * https://leetcode.com/problems/gas-station/description/
+   */
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+    int gasSum = 0;
+    int costSum = 0;
+    int tank = 0;
+    int start = 0;
+    for (int i = 0; i < gas.length; i++) {
+      gasSum += gas[i];
+      costSum += cost[i];
+      tank += (gas[i] - cost[i]);
+      if (tank < 0) {
+        tank = 0;
+        start = i + 1;
+      }
+    }
+    if (gasSum < costSum) {
+      return -1;
+    }
+    return start;
+  }
+
+  /**
    * https://leetcode.com/problems/surrounded-regions/description/
    */
   public void solve(char[][] board) {
