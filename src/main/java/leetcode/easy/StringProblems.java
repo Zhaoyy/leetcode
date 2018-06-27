c44f8301c821dd58a9500b0892cab2043f231f95
ZhaoYY <867986737@qq.com>
2018-06-27 14:04:07 +0800

    medium

--- src/main/java/leetcode/easy/StringProblems.java
+++ src/main/java/leetcode/easy/StringProblems.java
@@ -20,7 +20,32 @@ public class StringProblems {
     //    problems.reverseStr(
     //        "hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl",
     //        39));
-    System.out.println(problems.calculate("2*3-4"));
+    System.out.println(problems.longestValidParentheses(")()())"));
+  }
+
+  /**
+   * https://leetcode.com/problems/longest-valid-parentheses/description/
+   */
+  public int longestValidParentheses(String s) {
+    int longest = 0;
+    Stack<Integer> stack = new Stack<>();
+    stack.push(0);
+    for (char c : s.toCharArray()) {
+      if (c == '(') {
+        stack.push(0);
+      } else {
+        if (stack.size() > 1) {
+          int t = stack.pop();
+          stack.push(stack.pop() + t + 2);
+          longest = Math.max(longest, stack.peek());
+        } else {
+          stack.clear();
+          stack.push(0);
+        }
+      }
+    }
+
+    return longest;
   }
 
   /**
