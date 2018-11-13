27fc5ccdfc45a95d6d061060c727ab357fd12dfc
ZhaoYY <867986737@qq.com>
2018-11-13 09:39:39 +0800

    medium

--- src/main/java/leetcode/easy/TreeNodeProblems.java
+++ src/main/java/leetcode/easy/TreeNodeProblems.java
@@ -40,6 +40,16 @@ public class TreeNodeProblems {
   }
 
   /**
+   * https://leetcode.com/problems/range-sum-of-bst/description/
+   */
+  public int rangeSumBST(TreeNode root, int L, int R) {
+    if (root == null) return 0;
+    if (root.val < L) return rangeSumBST(root.right, L, R);
+    if (root.val > R) return rangeSumBST(root.left, L, R);
+    return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+  }
+
+  /**
    * https://leetcode.com/problems/maximum-binary-tree/description/
    */
   public TreeNode constructMaximumBinaryTree(int[] nums) {
