6d391689762372f15a73369f86fdea47b2cd4fbd
ZhaoYY <867986737@qq.com>
2018-11-30 09:52:42 +0800

    medium

--- src/main/java/leetcode/easy/TreeNodeProblems.java
+++ src/main/java/leetcode/easy/TreeNodeProblems.java
@@ -40,6 +40,21 @@ public class TreeNodeProblems {
   }
 
   /**
+   * https://leetcode.com/problems/binary-tree-pruning/description/
+   */
+  public TreeNode pruneTree(TreeNode root) {
+    if (root == null) return root;
+    root.left = pruneTree(root.left);
+    root.right = pruneTree(root.right);
+
+    if (root.val == 0 && root.left == null && root.right == null) {
+      return null;
+    } else {
+      return root;
+    }
+  }
+
+  /**
    * https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
    */
   public TreeNode insertIntoBST(TreeNode root, int val) {
