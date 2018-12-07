77e7bf0b50827acb6b44cabf61b6e213199819c4
ZhaoYY <867986737@qq.com>
2018-12-07 10:26:27 +0800

    medium

--- src/main/java/leetcode/easy/ArrayProblems.java
+++ src/main/java/leetcode/easy/ArrayProblems.java
@@ -51,6 +51,26 @@ public class ArrayProblems {
   }
 
   /**
+   * https://leetcode.com/problems/battleships-in-a-board/
+   */
+  public int countBattleships(char[][] board) {
+    if (board.length == 0) return 0;
+    int n = board[0].length;
+    int count = 0;
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < n; j++) {
+        if (board[i][j] == '.') continue;
+        if (i > 0 && board[i - 1][j] == 'X') continue;
+        if (j > 0 && board[i][j - 1] == 'X') continue;
+
+        count++;
+      }
+    }
+
+    return count;
+  }
+
+  /**
    * https://leetcode.com/problems/score-after-flipping-matrix/
    */
   public int matrixScore(int[][] A) {
