c7c4f2d638688e9d55bad3bf0870ebfc6fdbe7a4
ZhaoYY <867986737@qq.com>
2018-12-20 10:12:33 +0800

    medium

--- src/main/java/leetcode/easy/TreeNodeProblems.java
+++ src/main/java/leetcode/easy/TreeNodeProblems.java
@@ -41,6 +41,17 @@ public class TreeNodeProblems {
   }
 
   /**
+   * https://leetcode.com/problems/flip-equivalent-binary-trees/
+   */
+  public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+    if (root1 == null && root2 == null) return true;
+    if (root1 == null || root2 == null || root1.val != root2.val) return false;
+
+    return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
+        || flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+  }
+
+  /**
    * https://leetcode.com/problems/all-possible-full-binary-trees/
    */
   public List<TreeNode> allPossibleFBT(int N) {
