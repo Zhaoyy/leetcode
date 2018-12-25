23007398b2681eeeaa2e113f45532e320fb632bb
ZhaoYY <867986737@qq.com>
2018-12-25 14:14:42 +0800

    medium

---
+++ src/main/java/leetcode/easy/Node.java
@@ -0,0 +1,35 @@
+package leetcode.easy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Node {
+  public int val;
+  public List<Node> children;
+
+  public Node() {
+  }
+
+  public Node(int _val, List<Node> _children) {
+    val = _val;
+    children = _children;
+  }
+
+  /**
+   * https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+   */
+  public List<Integer> preorder(Node root) {
+    List<Integer> ans = new LinkedList<>();
+    preorderHelper(root, ans);
+    return ans;
+  }
+
+  private void preorderHelper(Node node, List<Integer> list) {
+    if (node == null) return;
+    list.add(node.val);
+    if (node.children == null || node.children.size() == 0) return;
+    for (Node child : node.children) {
+      preorderHelper(child, list);
+    }
+  }
+}
